bcd4a3646bcb6ac3833d48c53a63c04a4c65a696
chandan <chandan@relifems.com>
2020-01-21 16:16:32 +0530

    initial commit

---
+++ app/src/main/java/com/example/sliderdemo/MainActivity.java
@@ -0,0 +1,153 @@
+package com.example.sliderdemo;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager2.widget.ViewPager2;
+
+import android.os.Bundle;
+import android.text.Html;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.sliderdemo.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+    private ViewsSliderAdapter mAdapter;
+    private TextView[] dots;
+    private int[] layouts;
+    ActivityMainBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        init();
+    }
+
+    private void init() {
+        // layouts of all welcome sliders
+        // add few more layouts if you want
+        layouts = new int[]{
+                R.layout.slide_one,
+                R.layout.slide_two,
+                R.layout.slide_three};
+
+
+        mAdapter = new ViewsSliderAdapter();
+        binding.viewPager.setAdapter(mAdapter);
+        binding.viewPager.registerOnPageChangeCallback(pageChangeCallback);
+
+        binding.btnSkip.setOnClickListener(v -> launchHomeScreen());
+
+        binding.btnNext.setOnClickListener(v -> {
+            // checking for last page
+            // if last page home screen will be launched
+            int current = getItem(+1);
+            if (current < layouts.length) {
+                // move to next screen
+                binding.viewPager.setCurrentItem(current);
+            } else {
+                launchHomeScreen();
+            }
+        });
+
+
+        binding.btnNext.setOnClickListener(v -> {
+            // checking for last page
+            // if last page home screen will be launched
+            int current = getItem(+1);
+            if (current < layouts.length) {
+                // move to next screen
+                binding.viewPager.setCurrentItem(current);
+            } else {
+                launchHomeScreen();
+            }
+        });
+        addBottomDots(0);
+    }
+
+    private void addBottomDots(int currentPage) {
+        dots = new TextView[layouts.length];
+
+        int[] colorsActive = getResources().getIntArray(R.array.array_dot_active);
+        int[] colorsInactive = getResources().getIntArray(R.array.array_dot_inactive);
+
+        binding.layoutDots.removeAllViews();
+        for (int i = 0; i < dots.length; i++) {
+            dots[i] = new TextView(this);
+            dots[i].setText(Html.fromHtml("&#8226;"));
+            dots[i].setTextSize(35);
+            dots[i].setTextColor(colorsInactive[currentPage]);
+            binding.layoutDots.addView(dots[i]);
+        }
+
+        if (dots.length > 0)
+            dots[currentPage].setTextColor(colorsActive[currentPage]);
+    }
+    private int getItem(int i) {
+        return binding.viewPager.getCurrentItem() + i;
+    }
+    private void launchHomeScreen() {
+        Toast.makeText(this, "Slide Ended", Toast.LENGTH_LONG).show();
+        finish();
+    }
+    ViewPager2.OnPageChangeCallback pageChangeCallback = new ViewPager2.OnPageChangeCallback() {
+        @Override
+        public void onPageSelected(int position) {
+            super.onPageSelected(position);
+            addBottomDots(position);
+            // changing the next button text 'NEXT' / 'GOT IT'
+            if (position == layouts.length - 1) {
+                // last page. make button text to GOT IT
+                binding.btnNext.setText(getString(R.string.start));
+                binding.btnSkip.setVisibility(View.GONE);
+            } else {
+                // still pages are left
+                binding.btnNext.setText(getString(R.string.next));
+                binding.btnSkip.setVisibility(View.VISIBLE);
+            }
+        }
+    };
+
+    public class ViewsSliderAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+        public ViewsSliderAdapter() {
+        }
+
+        @NonNull
+        @Override
+        public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext())
+                    .inflate(viewType, parent, false);
+            return new SliderViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+
+        }
+
+        @Override
+        public int getItemViewType(int position) {
+            return layouts[position];
+        }
+
+        @Override
+        public int getItemCount() {
+            return layouts.length;
+        }
+
+        public class SliderViewHolder extends RecyclerView.ViewHolder {
+            public TextView title, year, genre;
+
+            public SliderViewHolder(View view) {
+                super(view);
+            }
+        }
+    }
+
+}
